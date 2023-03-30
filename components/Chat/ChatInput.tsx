e447e5f8296a1d69686ce1d47be7632a3f6462a8
Djalma Henrique <djalmahenry@outlook.com>
2023-03-30 08:17:29 -0300

    feat: modify text input

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -157,16 +157,16 @@ export const ChatInput: FC<Props> = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href="https://github.com/djalmahenry/difinity-chat"
           target="_blank"
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          Difinity Chat
         </a>
         .{' '}
         {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          "Difinity Chat is a model similar to ChatGPT for using the Open AI API. Enter your personal key to use the chat.",
         )}
       </div>
     </div>
